ab6acea44a4c9cc8374761acd1232b66097a9ec0
Andrey Demyanov <demyanov23@gmail.com>
2021-06-06 11:13:50 +0300

    added first components and props

--- project/src/index.js
+++ project/src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const film = {
+  name: 'The Grand Budapest Hotel',
+  type: 'Drama',
+  year: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      film={film}
+    />
   </React.StrictMode>,
-  document.getElementById('root'));
+  document.getElementById('root'),
+);
