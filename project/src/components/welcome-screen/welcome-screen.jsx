ec952009b9b356515fb29157a8307a861f89957d
Andrey Demyanov <demyanov23@gmail.com>
2021-06-09 11:34:01 +0300

    add all project pages and routing

--- project/src/components/welcome-screen/welcome-screen.jsx
+++ project/src/components/welcome-screen/welcome-screen.jsx
@@ -2,6 +2,8 @@ import {nanoid} from 'nanoid';
 import React from 'react';
 import PropTypes from 'prop-types';
 import FilmCard from '../film-card/film-card';
+import Logo from '../logo/logo';
+import Footer from '../footer/footer';
 
 function WelcomeScreen({film}) {
 
@@ -17,13 +19,7 @@ function WelcomeScreen({film}) {
         <h1 className="visually-hidden">WTW</h1>
 
         <header className="page-header film-card__head">
-          <div className="logo">
-            <a className="logo__link" href="_blank">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
+          <Logo />
 
           <ul className="user-block">
             <li className="user-block__item">
@@ -111,22 +107,10 @@ function WelcomeScreen({film}) {
 
         <div className="catalog__more">
           <button className="catalog__button" type="button">Show more</button>
-        </div>ЧЧт
+        </div>
       </section>
 
-      <footer className="page-footer">
-        <div className="logo">
-          <a className="logo__link logo__link--light" href="_blank">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
-        </div>
-
-        <div className="copyright">
-          <p>© 2019 What to watch Ltd.</p>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }
